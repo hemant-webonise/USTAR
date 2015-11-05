b7a43309732bbe9d4557eba25e57d84b8427066a
hemant-webonise <hemant.bonde@weboniselab.com>
2015-11-05 12:59:52 +0530

    Initial Commit

---
+++ USTAR/USTAR/NavigationStructure.xaml.cs
@@ -0,0 +1,12 @@
+﻿using USTAR.Presentation;
+
+namespace USTAR
+{
+    public sealed partial class NavigationStructure : NavigationItem
+    {
+        public NavigationStructure()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
