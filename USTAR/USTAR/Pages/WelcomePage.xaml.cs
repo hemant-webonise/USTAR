ef289f6f9846cceba41437cc220cfcf9511f0523
hemant-webonise <hemant.bonde@weboniselab.com>
2015-11-09 15:04:08 +0530

    Local Setting : (SharedPref in Android) Using Local setting and Giving you a small note about var.

--- USTAR/USTAR/Pages/WelcomePage.xaml.cs
+++ USTAR/USTAR/Pages/WelcomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System;
+using Windows.UI.Xaml.Controls;
 
 namespace USTAR.Pages
 {
@@ -7,20 +8,51 @@ namespace USTAR.Pages
     /// </summary>
     public sealed partial class WelcomePage : Page
     {
+        //We can not use var out the method so we had to use the reall data type esle var would to work 
+        //Note : var would'nt work here
+        Windows.Storage.ApplicationDataContainer localSettings;
+
         public WelcomePage()
         {
             this.InitializeComponent();
+            localSettings = Windows.Storage.ApplicationData.Current.LocalSettings;
+            UpdateLocalSetting();
         }
 
-        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        private void UpdateLocalSetting()
         {
-
+            // Read data from a simple setting
+            object value = localSettings.Values["Test"];
+            if (value == null)
+            {
+                // No data
+                Display.Text = "NO DATA";
+            }
+            else
+            {
+                // Access data in value
+                Display.Text = value.ToString();
+                DisplayPerson.Label = value.ToString();
+            }
         }
 
         private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {          
+
+           
+            // Create a simple setting
+            localSettings.Values["Test"] = Name.Text;                      
+        }
+
+        private void ClearData(object sender, Windows.UI.Xaml.RoutedEventArgs e)
         {
-            DisplayPerson.Label = "Hemant";
-            Display.Text = Name.Text;
+            // Delete a simple setting
+            localSettings.Values.Remove("Test");
+        }
+
+        private void Update(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            UpdateLocalSetting();
         }
     }
 }
